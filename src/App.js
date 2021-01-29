9ccee0dd6d5a42a38bd27a94d64ef971737cf74e
Kfir Arad <kfir.arad@naturalint.com>
2021-01-29 19:14:35 +0200

    create react app + matiral ui

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import MainForm from './components/MainForm/MainForm';
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <MainForm/>
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/MainForm/MainForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainForm(){
+    return (
+        <>
+            Hello World
+        </>
+    );
+}
